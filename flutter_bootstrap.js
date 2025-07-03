fe4cf38c1eab705438266f1454bbb4befb7f5dcc
hptechadmin <techadmin@hellopower.in>
2025-07-03 22:52:42 +0530

    New update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2701505087"
+    serviceWorkerVersion: "3153987358"
   }
 });
